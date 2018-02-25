0a6da635a33341c65ba35ac033d5d781a94ec047
vinarut <arut.73@gmail.com>
2018-02-25 21:25:08 +0300

    Навигационное меню и экран о приложении

---
+++ app/src/main/java/com/example/vladislav/menu/FragmentAboutApp.java
@@ -0,0 +1,25 @@
+package com.example.vladislav.menu;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by vladislav on 24.02.2018.
+ */
+
+public class FragmentAboutApp extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_about_app, null);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
