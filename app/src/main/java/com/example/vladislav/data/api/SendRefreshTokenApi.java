119f196a0b7b09b932295d0e652b61876f05f71a
dyadyaJora <ja_massa97@mail.ru>
2018-03-25 03:33:29 +0300

    bugfix (потом переделаю на @BODY)

--- app/src/main/java/com/example/vladislav/data/api/SendRefreshTokenApi.java
+++ app/src/main/java/com/example/vladislav/data/api/SendRefreshTokenApi.java
@@ -4,10 +4,11 @@ package com.example.vladislav.data.api;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.POST;
-import retrofit2.http.Field;
+import retrofit2.http.Query;
+
 
 public interface SendRefreshTokenApi {
 
     @POST("/api/tokens")
-    Call<ResponseBody> sendToken(@Field("registration_id") String token);
+    Call<ResponseBody> sendToken(@Query("registration_id") String token);
 }
